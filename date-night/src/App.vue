0e3a56c92acb9501562ae5736b39b714e17a3347
Peyton Critchfield <peyton.developer@gmail.com>
2021-05-05 11:48:07 -0400

    phase 1

--- date-night/src/App.vue
+++ date-night/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+
+    <div id="createNewTask">
+      <div class="column">Date Night Ideas</div>
+    </div>
+
+    <div>
+      Restaurants:
+    </div>
+
   </div>
 </template>
 
@@ -16,8 +25,20 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 100px;
+  max-width: 400px;
+  margin: 75px auto;
+}
+#createNewTask {
+  background-color: #13283bb6;
+  font-size: 22px;
+  color: rgba(255, 255, 255, 0.815);
+  margin: 1em auto;
+  border-radius: 10px;
+  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
